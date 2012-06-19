f666c98c88e1b8e21246be9f38ec8b3b6379f76c
mose <mose@mose.tw>
2012-06-20 01:45:51 +0800

    test fix again

--- lib/cogbot/callback_handler.rb
+++ lib/cogbot/callback_handler.rb
@@ -2,14 +2,13 @@ module Cogbot
   class CallbackHandler
     include Cinch::Plugin
 
-    listen :api_callback
+    listen_to :api_callback
 
     def listen(m, json_in)
       json = StringIO.new(json_in)
       parser = Yajl::Parser.new(:symbolize_keys => true)
       hash = parser.parse(json)
       config['main']['channels'].each do |channel|
-        files, additions, subtractions = get_commit_details(changeset)
         hash[:commits].each do |c|
           Channel(channel).send "[%s:%s] %s <%s>" % [
             hash[:repository][:name],
