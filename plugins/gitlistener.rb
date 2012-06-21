45a4a156c869503dfd6af0264a91a397f736e687
mose <mose@codegreenit.com>
2012-06-21 13:40:33 +0800

    fixed gitlistener

--- plugins/gitlistener.rb
+++ plugins/gitlistener.rb
@@ -10,14 +10,16 @@ module Cinch
       def listen(m, json)
         hash = Yajl::Parser.parse(URI.unescape(json[8..-1]))
         #bot.loggers.debug(hash.inspect)
-        config['main']['channels'].each do |channel|
+        @bot.config.options['cogconf']['main']['channels'].each do |channel|
         #['#dev'].each do |channel|
-          Channel(channel).msg "%s@%s: %s [ %s ]" % [
-            hash['pusher']['name'],
-            hash['repository']['name'],
-            hash['head_commit']['message'],
-            hash['head_commit']['modified'].join(', ')
-          ]
+          hash['commits'].each do |c|
+            Channel(channel).msg "%s@%s: %s [ %s ]" % [
+              c['committer']['name'],
+              hash['repository']['name'],
+              c['message'],
+              c['modified'].join(', ')
+            ]
+          end
         end
       end
     end
