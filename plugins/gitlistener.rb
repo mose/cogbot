0ed95622ff0968b3dc2ec60a2b5391a9e651d9c9
mose <mose@codegreenit.com>
2012-06-23 04:17:05 +0800

    split status of files

--- plugins/gitlistener.rb
+++ plugins/gitlistener.rb
@@ -1,3 +1,5 @@
+require "cgi"
+
 module Cinch
   module Plugins
     class Gitlistener
@@ -9,15 +11,19 @@ module Cinch
 
       def listen(m, json)
         hash = Yajl::Parser.parse(URI.unescape(json[8..-1]))
-        #bot.loggers.debug(hash.inspect)
+        bot.loggers.debug(hash.inspect)
         @bot.config.options['cogconf']['main']['channels'].each do |channel|
         #['#dev'].each do |channel|
           hash['commits'].each do |c|
-            Channel(channel).msg "%s@%s: %s [ %s ]" % [
+            files = ""
+            files += '[= ' + c['modified'].join(' ') + ' ] ' unless c['modified'].empty?
+            files += '[- ' + c['removed'].join(' ') + ' ] ' unless c['removed'].empty?
+            files += '[+ ' + c['added'].join(' ') + ' ] ' unless c['added'].empty?
+            Channel(channel).msg "%s@%s: %s %s" % [
               c['committer']['name'],
               hash['repository']['name'],
-              c['message'],
-              c['modified'].join(', ')
+              CGI::unescape(c['message']),
+              files
             ]
           end
         end
