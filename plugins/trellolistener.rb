d09084a51f71a6f5c39366c3c39d4caa3146fad3
mose <mose@mose.com>
2015-01-31 16:32:21 +0800

    prettifying

--- plugins/trellolistener.rb
+++ plugins/trellolistener.rb
@@ -13,52 +13,52 @@ module Cinch
         hash = Yajl::Parser.parse(URI.unescape(json))
         bot.loggers.debug(hash.inspect)
         if  @bot.config.options['cogconf']['trello']
-        @bot.config.options['cogconf']['trello']['announce'].each do |channel|
-          action = hash['action']['type']
-	  case action
-	  when 'createCard'
-          Channel(channel).msg "%s %s created in %s: %s %s" % [
-            Format(:yellow, "[%s]" % hash['action']['data']['board']['name']),
-            Format(:aqua, hash['action']['memberCreator']['username']),
-            Format(:orange, hash['action']['data']['list']['name']),
-            hash['action']['data']['card']['name'],
-            Format(:grey, "(%s)" % link(hash['action']['data']['card']['shortLink']))
-          ]
-	  when 'updateCard'
-          Channel(channel).msg "%s %s moved in %s: %s %s" % [
-            Format(:yellow, "[%s]" % hash['action']['data']['board']['name']),
-            Format(:aqua, hash['action']['memberCreator']['username']),
-            Format(:orange, hash['action']['data']['list']['name']),
-            hash['action']['data']['card']['name'],
-            Format(:grey, "(%s)" % link(hash['action']['data']['card']['shortLink']))
-          ]
-	  when 'addLabelToCard'
-          Channel(channel).msg "%s %s labelled as %s: %s %s" % [
-            Format(:yellow, "[%s]" % hash['action']['data']['board']['name']),
-            Format(:aqua, hash['action']['memberCreator']['username']),
-            Format(:green, hash['action']['data']['label']['name']),
-            hash['action']['data']['card']['name'],
-            Format(:grey, "(%s)" % link(hash['action']['data']['card']['shortLink']))
-          ]
-	  when 'removeLabelFromCard'
-          Channel(channel).msg "%s %s unlabelled as %s: %s %s" % [
-            Format(:yellow, "[%s]" % hash['action']['data']['board']['name']),
-            Format(:aqua, hash['action']['memberCreator']['username']),
-            Format(:grey, hash['action']['data']['label']['name']),
-            hash['action']['data']['card']['name'],
-            Format(:grey, "(%s)" % link(hash['action']['data']['card']['shortLink']))
-          ]
-	  when 'commentCard'
-          Channel(channel).msg "%s %s commented on \"%s\" in %s: %s %s" % [
-            Format(:yellow, "[%s]" % hash['action']['data']['board']['name']),
-            Format(:aqua, hash['action']['memberCreator']['username']),
-            truncate(hash['action']['data']['card']['name']),
-            Format(:orange, hash['action']['data']['list']['name']),
-            truncate(hash['action']['data']['text']),
-            Format(:grey, "(%s)" % link(hash['action']['data']['card']['shortLink']))
-          ]
-	  end
-        end
+          @bot.config.options['cogconf']['trello']['announce'].each do |channel|
+            action = hash['action']['type']
+        	  case action
+        	  when 'createCard'
+              Channel(channel).msg "%s %s created in %s: %s %s" % [
+                Format(:yellow, "[%s]" % hash['action']['data']['board']['name']),
+                Format(:aqua, hash['action']['memberCreator']['username']),
+                Format(:orange, hash['action']['data']['list']['name']),
+                truncate(hash['action']['data']['card']['name']),
+                Format(:grey, "(%s)" % link(hash['action']['data']['card']['shortLink']))
+              ]
+        	  when 'updateCard'
+              Channel(channel).msg "%s %s moved in %s: %s %s" % [
+                Format(:yellow, "[%s]" % hash['action']['data']['board']['name']),
+                Format(:aqua, hash['action']['memberCreator']['username']),
+                Format(:orange, hash['action']['data']['list']['name']),
+                truncate(hash['action']['data']['card']['name']),
+                Format(:grey, "(%s)" % link(hash['action']['data']['card']['shortLink']))
+              ]
+        	  when 'addLabelToCard'
+              Channel(channel).msg "%s %s labelled as %s: %s %s" % [
+                Format(:yellow, "[%s]" % hash['action']['data']['board']['name']),
+                Format(:aqua, hash['action']['memberCreator']['username']),
+                Format(:green, hash['action']['data']['label']['name']),
+                truncate(hash['action']['data']['card']['name']),
+                Format(:grey, "(%s)" % link(hash['action']['data']['card']['shortLink']))
+              ]
+        	  when 'removeLabelFromCard'
+              Channel(channel).msg "%s %s unlabelled as %s: %s %s" % [
+                Format(:yellow, "[%s]" % hash['action']['data']['board']['name']),
+                Format(:aqua, hash['action']['memberCreator']['username']),
+                Format(:grey, hash['action']['data']['label']['name']),
+                truncate(hash['action']['data']['card']['name']),
+                Format(:grey, "(%s)" % link(hash['action']['data']['card']['shortLink']))
+              ]
+        	  when 'commentCard'
+              Channel(channel).msg "%s %s commented on \"%s\" in %s: %s %s" % [
+                Format(:yellow, "[%s]" % hash['action']['data']['board']['name']),
+                Format(:aqua, hash['action']['memberCreator']['username']),
+                truncate(hash['action']['data']['card']['name']),
+                Format(:orange, hash['action']['data']['list']['name']),
+                truncate(hash['action']['data']['text']),
+                Format(:grey, "(%s)" % link(hash['action']['data']['card']['shortLink']))
+              ]
+        	  end
+          end
         end
       end
 
