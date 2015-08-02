28e10b8826489042249a22bd3d79ecea8fa95693
mose <mose@mose.com>
2015-08-03 00:30:35 +0800

    more doc

--- lib/cogbot/utils.rb
+++ lib/cogbot/utils.rb
@@ -1,5 +1,8 @@
+# this is a monkey patching over the String class 
+# to bring the camelcase method from rails
 class String
 
+  # the famous camelize method so useful in metaprogramming
   def camelize
     self.downcase!
     self.gsub!(/(?:_| )?(.)([^_ ]*)/) { "#{$1.upcase}#{$2}" }
