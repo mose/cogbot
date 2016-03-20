d201b2045de7701294d00072e4d98ef41a74381b
mose <mose@mose.com>
2016-03-20 15:35:47 +0800

    typoes

--- plugins/rss.rb
+++ plugins/rss.rb
@@ -12,15 +12,20 @@ The RSS plugin will poll rss feed every 5 minutes
 EOT
 
       timer 300, method: :fetch_rss
-#      timer @bot.config.options['rss']['polling'] || 300, method: fetch_rss
+
+      def new(bot)
+        @bot = bot
+        @feeds = []
+      end
 
       def fetch_rss
+        @feeds ||= []
         open(@bot.config.options['cogconf']['rss']['channel']) do |rss|
           feed = RSS::Parser.parse(rss)
           feed.items.reverse.each do |item|
             unless @feeds.include? item.link
               @feeds << item.link
-              @feed.shift if @feed.length > 10
+              @feeds.shift if @feeds.length > 10
               @bot.config.options['cogconf']['rss']['announce'].each do |announce|
                 Channel(announce).send "#{item.title} (#{item.link})"
               end
