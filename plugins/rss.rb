333db8aa49a24d25bfff2fe3e9a8c5a00b9e8a23
mose <mose@mose.com>
2016-03-21 07:11:12 +0800

    debugging and adjustments

--- plugins/rss.rb
+++ plugins/rss.rb
@@ -22,16 +22,23 @@ EOT
         @feeds ||= []
         open(@bot.config.options['cogconf']['rss']['channel']) do |rss|
           feed = RSS::Parser.parse(rss)
+          p @feeds.length
+          p feed.items.length
           feed.items.reverse.each do |item|
+            p item.link
             unless @feeds.include? item.link
               @feeds << item.link
-              if @feeds.length > 10
-                @feeds.shift
+              p @feeds.length
+              # to prevent the first run displays all the items
+              if @feeds.length > feed.items.length
                 @bot.config.options['cogconf']['rss']['announce'].each do |announce|
                   Channel(announce).send "#{item.title} (#{item.link})"
                 end
               end
             end
+          end
+          if @feeds.length > feed.items.length
+            @feeds = @feeds[-feed.items.length, feed.items.length]
           end
         end
       end
